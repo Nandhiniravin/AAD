d7eb2137c580a1ad53e6fac5943ea75651c6f3b8
Nandhini <nandhiniravindran167@gmail.com>
2024-03-20 13:39:05 +0530

    Day 3.

--- FRONTEND_AAD/ZeeVa_Booking/ZeeVa/src/App.jsx
+++ FRONTEND_AAD/ZeeVa_Booking/ZeeVa/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-//import './App.css'
 import React from "react";
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import MainMenu from './MainMenu';
